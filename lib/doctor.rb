f2e9c9177a403e294d7fe506d0212949e395b37e
Justin Breneman <justin.breneman@yahoo.com>
2020-06-19 01:33:21 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,4 +8,10 @@ class Doctor
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  def patients
+    appointments.map { |appointment| appointment.patient }
+  end
 end
